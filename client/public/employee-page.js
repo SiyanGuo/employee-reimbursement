c2a45312403e8711322a7349dc3be371f117ed86
Serena Guo <syg9345@gmail.com>
2022-03-23 23:40:01 -0400

    add front end to get all reimbursements for specific employee

--- client/public/employee-page.js
+++ client/public/employee-page.js
@@ -1,7 +1,7 @@
 let mobileBtn = document.querySelector('#mobile-btn');
 let mobileMenu = document.querySelector('#mobile-menu');
 let logoutBtn = document.querySelector('.logout-btn');
-
+let reimbursementCtn = document.querySelector('#reimbursement-container')
 
 logoutBtn.addEventListener('click', () => {
     localStorage.removeItem('jwt');
@@ -30,6 +30,71 @@ async function populateReimbursements() {
 
         for (let reimbursement of reimbursements) {
 
+            let card = document.createElement('div');
+            card.classList.add('bg-white', 'rounded-lg', 'px-4', 'py-4', 'drop-shadow-md');
+
+            let status = document.createElement('h2');
+            status.classList.add('tracking-widest', 'text-xl', 'pb-3', 'font-semibold');
+            status.innerText= reimbursement.status;
+
+            let id= document.createElement('h2');
+            id.classList.add('tracking-widest', 'text-purple');
+            id.innerText = `Id: ${reimbursement.id}`;
+
+            let amount= document.createElement('h2');
+            amount.classList.add('tracking-widest', 'text-purple');
+            amount.innerText = `Amount: ${reimbursement.amount}`;
+
+            let type= document.createElement('h2');
+            type.classList.add('tracking-widest', 'text-purple');
+            type.innerText = `Type: ${reimbursement.type}`;
+
+            let description= document.createElement('h2');
+            description.classList.add('tracking-widest', 'text-purple');
+            description.innerText = `Description: ${reimbursement.description}`;
+
+            let submitAt= document.createElement('h2');
+            submitAt.classList.add('tracking-widest', 'text-purple');
+            submitAt.innerText = `Submitted at: ${reimbursement.submittedAt}`;
+
+            let resolvedAt= document.createElement('h2');
+            resolvedAt.classList.add('tracking-widest', 'text-purple');
+            
+            let resolvedBy= document.createElement('h2');
+            resolvedBy.classList.add('tracking-widest', 'text-purple');
+            
+            let receipt = document.createElement('a');
+            receipt.classList.add('font-semibold', 'text-purple', 'hover:text-bubble-gum');
+            receipt.setAttribute('href', `${reimbursement.receipt}`);
+            receipt.setAttribute('target', '_blank');
+            receipt.innerText= "View Receipt";
+       
+            if(reimbursement.status == 'APPROVED'){
+                status.classList.add('text-purple');
+                resolvedAt.innerText = `Resolved at: ${reimbursement.resolvedAt}`
+                resolvedBy.innerText = `Resolved by: ${reimbursement.resolver.firstName} ${reimbursement.resolver.lastName}`
+            } else if (reimbursement.status == 'PENDING') {
+                status.classList.add('text-bubble-gum');
+                resolvedAt.innerText = "PENDING";
+                resolvedBy.innerText = "PENDING";
+            } else {
+                status.classList.add('text-tahiti-blue');
+                resolvedAt.innerText = `Resolved at: ${reimbursement.resolvedAt}`
+                resolvedBy.innerText = `Resolved by: ${reimbursement.resolver.firstName} ${reimbursement.resolver.lastName}`;
+            }
+
+            card.appendChild(status);
+            card.appendChild(id);
+            card.appendChild(amount);
+            card.appendChild(type);
+            card.appendChild(description);
+            card.appendChild(submitAt);
+            card.appendChild(resolvedAt);
+            card.appendChild(resolvedBy)
+            card.appendChild(receipt);
+
+            reimbursementCtn.appendChild(card);
+       
         }
 
 
